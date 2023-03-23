ccadbad71ce66663d1639aea6dac4aeb7076c1cd
typoworx-de <info@typoworx.de>
2023-03-23 15:45:58 +0100

    Create Promise.php

---
+++ logic/promise-resolver/Promise.php
@@ -0,0 +1,165 @@
+<?php
+declare(strict_types=1);
+namespace TYPOworx\FwfSearchbar\Api\Promise;
+
+/**
+ * Promise/Resolver
+ * 
+ * Promise::create()
+ *   ->resolver(function() use($request, $options) {
+ *       return $request->apiRequest($options);
+ *   })
+ *   ->then(function() {
+ *      // Proceed on success
+ *   })
+ *   ->rejected(function() {
+ *      // React on reject
+ *   })
+ *   ->catch(function($exception) {
+ *    var_dump($exception);
+ *   })
+ *  ->resolve()
+ * ;
+ */
+class Promise implements PromiseInterface
+{
+    private bool $propagadeRejected = false;
+    private bool $propagadeSuccessful = false;
+
+    /** @var callable */
+    private $callableResolver;
+
+    /** @var callable */
+    private $callbackThen;
+
+    /** @var callable|null */
+    private $callbackReject;
+
+    /** @var callable|null */
+    private $callbackCatch;
+
+    /** @var callable|null */
+    private $callbackFinally;
+
+    private mixed $result = null;
+
+
+    public static function create() : self
+    {
+        return new (static::class)();
+    }
+
+    public function then(?callable $onFulfilled = null, ?callable $onRejected = null) : self
+    {
+        $this->callbackThen = $onFulfilled;
+        $this->callbackReject = $onRejected;
+
+        return $this;
+    }
+
+    public function reject(callable $onRejected) : self
+    {
+        $this->callbackReject = $onRejected;
+
+        return $this;
+    }
+
+    public function catch(callable $onCatchException) : self
+    {
+        $this->callbackCatch = $onCatchException;
+
+        return $this;
+    }
+
+    public function finally(callable $onFulfilledOrRejected) : self
+    {
+        $this->callbackFinally = $onFulfilledOrRejected;
+
+        return $this;
+    }
+
+    private function call($callable, mixed ...$arguments) : self
+    {
+        if (is_callable($callable))
+        {
+            $return = \Closure::fromCallable($callable)
+                ->call($this, $arguments)
+            ;
+
+            if ($callable instanceof $this->callableResolver)
+            {
+                $this->result = $return;
+            }
+        }
+
+        return $this;
+    }
+
+    private function propagadeSuccessful() : void
+    {
+        $this->propagadeSuccessful = true;
+        $this->propagadeRejected = false;
+    }
+
+    private function isSuccessful() : bool
+    {
+        return $this->propagadeSuccessful;
+    }
+
+    private function propagadeRejected() : void
+    {
+        $this->propagadeSuccessful = false;
+        $this->propagadeRejected = true;
+
+        throw new RejectedThrowable();
+    }
+
+    private function isRejected() : bool
+    {
+        return $this->propagadeRejected;
+    }
+
+    public function resolver(callable $resolver) : self
+    {
+        $this->callableResolver = $resolver;
+
+        return $this;
+    }
+
+    public function resolve() : void
+    {
+        try
+        {
+            $this->call($this->callableResolver);
+
+            if ($this->result || $this->propagadeSuccessful = true)
+            {
+                $this->call($this->callbackThen);
+            }
+            else
+            {
+                $this->call($this->callbackReject);
+            }
+
+            $this->call($this->callbackFinally);
+        }
+        catch (RejectedThrowable $e)
+        {
+            $this->call($this->callbackReject);
+        }
+        catch (\Throwable $e)
+        {
+            if ($this->callbackCatch)
+            {
+                $this->call($this->callbackCatch, $e);
+            }
+
+            throw $e;
+        }
+    }
+
+    public function getResult() : mixed
+    {
+        return $this->result;
+    }
+}
